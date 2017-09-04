81f696f7dd7331d62199e1aa1568ee51aced193f
Julien Duval <julienduval111@gmail.com>
2017-09-05 01:06:39 +0200

    Read response text and send it to slack

--- functions/shared/customApiAiApp.js
+++ functions/shared/customApiAiApp.js
@@ -10,12 +10,37 @@ class CustomApiAiApp extends ApiAiApp {
     buildResponse_(textToSpeech, expectUserResponse, noInputs) {
         const response = super.buildResponse_(textToSpeech, expectUserResponse, noInputs);
 
-        // TODO add only when applicable
-        // originalRequest.source === 'slack_testbot' ???
-        // result.contexts[name=generic].parameters.slack_user_id is present ??
-        response.data.slack = { 'text': 'Hello from slack!' };
+        if (response) {
+            if (this.isFromSlack()) {
+                // https://api.slack.com/docs/message-formatting
+                // https://api.slack.com/docs/message-attachments
+                // https://api.slack.com/interactive-messages
+                const slackResponse = {
+                    text: response.speech
+                };
+
+                if (response.data && response.data.google && response.data.google.rich_response) {
+                    const richResponse = response.data.google.rich_response;
+
+                    if (richResponse.items && richResponse.items[0] && richResponse.items[0].simple_response) {
+                        slackResponse.text = richResponse.items[0].simple_response.text_to_speech;
+                    }
+
+                    if (richResponse.suggestions && richResponse.suggestions.length) {
+                        // it may be possible to do things here but slack need more info than provided by richResponses
+                    }
+                }
+
+                response.data.slack = slackResponse;
+            }
+        }
 
         return response;
+    }
+
+    /** @return {boolean} return true if current intent comes from a slack bot  */
+    isFromSlack() {
+        return !!this.getContextArgument('generic', 'slack_user_id');
     }
 }
 
